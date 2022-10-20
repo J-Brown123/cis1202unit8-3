bd949127e0d916bf9b4b5ed55e01fdab43a59023
Jordan Brown <jordan.brown@sinclair.edu>
2022-10-20 16:05:35 -0400

    Int function

--- cis1202unit8-3/cis1202unit8-3.cpp
+++ cis1202unit8-3/cis1202unit8-3.cpp
@@ -6,6 +6,11 @@
 
 using namespace std;
 
+//Function Prototypes
+template <typename T>
+T half(T num);
+int half(int);
+
 int main()
 {
 
@@ -18,5 +23,12 @@ T half(T num)
 	T newNum;
 	newNum = num / 2;
 
+	return newNum;
+}
+
+int half(int num)
+{
+	int newNum;
+	newNum = round(static_cast<double>(newNum / 2.00));
 	return newNum;
 }
